18a15c14401a5d3742709746aeca9861dd9ca91d
agudu50 <anthonygudu2020@gmail.com>
2024-12-21 11:54:52 +0000

    updated

--- com/rentalmanagement/vehicles/Motorcycle.java
+++ com/rentalmanagement/vehicles/Motorcycle.java
@@ -1,40 +1,29 @@
 package com.rentalmanagement.vehicles;
 
 public class Motorcycle extends Vehicle {
-    private boolean hasHelmetIncluded;
+    private final boolean hasHelmetIncluded;
 
-    public Motorcycle (String vehicleId, String model, double baseRentalRate, boolean hasHelmetInclude) {
+    public Motorcycle(String vehicleId, String model, double baseRentalRate, boolean hasHelmetIncluded) {
         super(vehicleId, model, baseRentalRate);
+        this.hasHelmetIncluded = hasHelmetIncluded; // Correct assignment
     }
-
 
     @Override
     public double calculateRentalCost(int days) {
         double cost = getBaseRentalRate() * days;
         if (hasHelmetIncluded) {
-            cost += cost;
+            cost += 20; // Add extra cost for the helmet
         }
-
         return cost;
     }
 
-
     @Override
     public boolean isAvailableForRental() {
-        return isAvailable();
-
+        return isAvailable(); // Ensure `isAvailable` is implemented in Vehicle
     }
 
     @Override
     public String toString() {
-        return super.toString() +", hasHelmetIncluded=" +hasHelmetIncluded;
+        return super.toString() + ", hasHelmetIncluded=" + hasHelmetIncluded;
     }
-
-
-
-
-
-
-
-
 }
